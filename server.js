490fd1b108a544af56979df159163d3207b56ca2
kyrollos-iskandar <21704248@students.latrobe.edu.au>
2024-08-16 22:24:56 +1000

    nodemon

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const app = express();
 
 // Specify how to respond to GET /
 app.get('/', (req, res) => {
-    res.send('Hello world! ' + Date());
+    res.send('Hello world! ' + Date() + ' Your fortune is ...');
 })
+// NOTE: Using <br></br> in res.send() caused nodemon to crash. Removing that HTML tag restarted nodemon.
 
 // Start listening for HTTP requests on port 3000
 app.listen(3000, () => {
